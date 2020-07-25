6f1477dd0af407a94ba11a5f59b49142d600abe2
MarcosPimienta <1676@holbertonschool.com>
2020-07-25 00:53:48 +0000

    fixed printf

--- holberton.h
+++ holberton.h
@@ -9,7 +9,7 @@
 
 int _putchar(char c);
 int print_char(va_list lista);
-int print_percent(va_list lista);
+int print_percent(__attribute__((unused))va_list lista);
 int print_str(va_list lista);
 int _printf(const char *format, ...);
 int putN(unsigned int n, unsigned int b, char *nums);
--- printf.c
+++ printf.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-
 /**
 *_printf - print char at printf
 *@format: the main string passed to the function
@@ -9,32 +8,31 @@ int _printf(const char *format, ...)
 {
 		va_list Lista_Of_argument;
 		int Bits = 0;
-
 		va_start(Lista_Of_argument, format);
 		while (format && *format)
 		{
 			if (*format == '%')
 			{
 				format++;
-				if (*format)
+				if (*format == ' ' || *format == '\t')
 				{
-				if (*format == 'c')
-					format++, Bits += print_char(Lista_Of_argument);
+					while (((*format) == ' ') || ((*format) == '\t'))
+					format++;
+				}
+				if (*format == '%')
+					Bits += print_percent(Lista_Of_argument), format++;
 				else if (*format == 's')
-					format++, Bits += print_str(Lista_Of_argument);
-				else if (*format == '%')
-					format++, Bits += print_percent(Lista_Of_argument);
+					Bits += print_str(Lista_Of_argument), format++;
+				else if (*format == 'c')
+					Bits += print_char(Lista_Of_argument), format++;
 				else if (*format == 'd')
 					format++, Bits += print_int(Lista_Of_argument);
 				else if (*format == 'i')
 				format++, Bits += print_int(Lista_Of_argument);
-				else if (*format == ' ' && *format++ == '%')
-				format++, Bits += print_percent(Lista_Of_argument);
 				else
 				{
-					Bits += print_percent(Lista_Of_argument);
-					write(1, &(*format++), 1);
-				}
+					write(1, "%%", 1);  
+					Bits++;
 				}
 			}
 			else
